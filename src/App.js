507b5a34dd0f5a79d786de030768e848550610f4
Chinemerem Ugbaja <marycynthiaconfirm@gmail.com>
2023-04-27 04:15:21 +0100

    my commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Forms from "../../form-with-react/src/component/Forms";
+import axios from "axios";
+import { nanoid } from "nanoid";
 
 function App() {
+  const pushToAPI = async (form) => {
+    const post = {
+      id: nanoid(),
+      ...form,
+    };
+    try {
+      const res = await axios.post(
+        "https://my-json-server.typicode.com/tundeojediran/contacts-api-server/inquiries",
+        post
+      );
+      console.log(res.data);
+      alert(res);
+    } catch (e) {
+      alert(e);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Forms submitForm={pushToAPI} />
     </div>
   );
 }
